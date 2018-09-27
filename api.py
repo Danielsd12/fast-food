c05efe837da4284bdf1729dd588e8f18aac4de95
Danielsd12 <danserunkuma3@gmail.com>
2018-09-27 15:28:58 +0300

    add files

--- api.py
+++ api.py
@@ -1,44 +1,61 @@
 from flask import Flask, request, jsonify, json
 app = Flask(__name__)
-orders = [{'orderid': 1, 'name': 'Sserunkuma Daniel','residence':'Lubaga',  'item':'chipse',   'price':23000},
-           {'orderid': 2, 'name': 'Nagawa Ruth',     'residence':'mengo',     'item':'chicken','price':50000},
-           {'orderid': 3, 'name': 'Namata Recheal',  'residence':'nakawa',   'item':'pizza',   'price':20000},
-           {'orderid': 4, 'name': 'Sentanlo John',   'residence':'makindye',  'item':'coffe',   'price':5500},
-           {'orderid': 5, 'name': 'John Bosco',      'residence':'kampala',   'item':'beaf',     'price':35000},
-           {'orderid': 6, 'name': 'Omega Lutalo',    'residence':'namungoona',  'item': 'gollilos','price':12000},
-           {'orderid': 7, 'name': 'Senyondo Robert', 'residence':'kamwokya',  'item':'hot dog',    'price':90000}]
+
+
+''' i have created a list as a data structure that will help
+ me save or hold my data that has been 
+appended to the list and this will finally work as my datastore.'''
+
+
+orders = [{'orderid':1 , 'name': 'molly', 'residence':'kasubi',  'item':'chipse',   'price':'4500'}]
+ 
+'''
+implimenting a route , this route undertakes two methods. these include 
+the POST method and the GET method.
+  These will be my end points to or headers to run the get and post functionalities in my fast-food 
+  application. 
+  '''
+
 @app.route('/api/v1/order', methods=('GET', 'POST'))
 def order():
     if request.method == 'GET':
         response = orders
-    else:  # POST
+
+        #### POST ######
+    else:  
         if request.content_type != 'application/json':
             return jsonify({}),200
         try:
             new_orders = json.loads(request.data)
         except ValueError:
             return jsonify({})
-            ########     code execeeds this level or reaches here
+
         print("before if")
         if 'orderid' in new_orders and isinstance(new_orders['orderid'], int) and \
            'name' in new_orders and isinstance(new_orders['name'], str) and \
            'residence' in new_orders and isinstance(new_orders['residence'],str) and\
            'item' in new_orders and isinstance(new_orders['item'],str) and\
-           'price' in new_orders and isinstance(new_orders['price'],int) and\
+         'price' in new_orders and isinstance(new_orders['price'],int) and\
            len(new_orders.keys()) == 5 and new_orders not in orders:
            print("after if")
            orders.append(new_orders)
            response = orders[-1]
         else:
-            ###### am testing to see whether my code run upto here  ######
+
+            ###### am testing to see whether my code run upto here  #######
+
             return jsonify("The Validation Failed")
     return jsonify(response)
+
+
 @app.route('/api/v1/order/<int:orderid>', methods=('PUT', 'DELETE'))
+
 def update_orders(orderid):
+
     if request.method == 'DELETE':
         del orders [orderid]
         return jsonify(orderid)
-        
+
     else: 
         orders[orderid] = json.loads(request.data)
         return jsonify({})
--- test_app.py
+++ test_app.py
@@ -8,7 +8,31 @@ class Test_Order(unittest.TestCase):
         self.tester = app.test_client(self)
 
 
-    def test_order(self):
+    def test_get_orders(self):
         response = self.tester.get('/api/v1/order')
         self.assertEqual(response.status_code,200)
-        
+
+    ''' testing a single order '''
+    def test_get_one_order(self):
+        response = self.tester.get('/api/v1/order/1')
+        self.assertEqual(response.status_code,200)    
+
+
+    
+
+        ##################################   end of my first test ####################
+
+
+    def Test_Orderid(self,orderid):
+        if orderid == str():
+            return True
+        elif orderid == int():
+            return false
+
+    class order_orderid(self,TestCase):
+        if request.method == "DELETE":
+            responce = self.tester.delete('/api/vi/orderid')     
+        else:
+            print("new order has been deleted")
+
+########################   DELETE TEST     ####################
